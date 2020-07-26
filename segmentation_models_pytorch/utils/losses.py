ea5b5cca20a643720bbccafbfd3ba2b988e2af1e
justina-1015 <56578216+justina-1015@users.noreply.github.com>
2020-07-26 12:15:00 +0800

    Update losses.py

--- segmentation_models_pytorch/utils/losses.py
+++ segmentation_models_pytorch/utils/losses.py
@@ -21,7 +21,21 @@ class JaccardLoss(base.Loss):
             threshold=None,
             ignore_channels=self.ignore_channels,
         )
+   
 
+class Focalloss(base.Loss):
+
+    def __init__(self, eps=1., alpha=0.5, gamma=2, activation=None, ignore_channels=None, **kwargs):
+        super().__init__(**kwargs)
+        self.eps = eps
+        self.alpha = alpha
+        self.gamma = gamma
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        return - self.alpha * (1 - y_pr) ** self.gamma * y_gt * torch.log(y_pr) - (1 - self.alpha) * v ** self.gamma * (1 - y_gt) * torch.log(1 - y_pr)
 
 class DiceLoss(base.Loss):
 
